3c3135eae5a4d52eb24d8c83a6dd2f53aab433fd
valentinarizzo21 <rizzo.val3ntina@gmail.com>
2025-01-08 20:43:03 +0100

    modal modified

--- giorno-2/src/components/MyAlert.jsx
+++ giorno-2/src/components/MyAlert.jsx
@@ -1,26 +1,57 @@
-import { Container, Row } from "react-bootstrap";
-import Alert from "react-bootstrap/Alert";
-
+import React, { useState } from "react";
+import { Container, Row, Modal, Alert, Button } from "react-bootstrap";
 
 function MyAlert() {
+  const [showModal, setShowModal] = useState(false);
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+  
   return (
     <Container className="my-5">
       <Row>
         <Alert variant="primary" className="my-5">
           <Alert.Heading>Hey, nice to see you again!</Alert.Heading>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum corporis quam esse, accusantium, totam iusto fuga porro pariatur ullam dolor rem quaerat ab placeat, animi amet numquam! Ex, provident dignissimos?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium commodi perferendis asperiores odit eaque? Enim at est assumenda illo cupiditate exercitationem debitis delectus omnis culpa, dolore harum totam tenetur cum?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
+            corporis quam esse, accusantium, totam iusto fuga porro pariatur
+            ullam dolor rem quaerat ab placeat, animi amet numquam! Ex,
+            provident dignissimos? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Accusantium commodi perferendis asperiores odit
+            eaque? Enim at est assumenda illo cupiditate exercitationem debitis
+            delectus omnis culpa, dolore harum totam tenetur cum?
           </p>
           <hr />
           <h4 className="mb-0">
             Courious to see the offers? Don&apos;t hesitate and click here!
           </h4>
           <div className="d-flex justify-content-center">
-          <button type="button" className="btn btn-warning my-2 py-3 px-4">See Offers!</button>
+            <button
+              type="button"
+              className="btn btn-warning my-2 py-3 px-4"
+              onClick={handleShow}
+            >
+              See Offers!
+            </button>
           </div>
         </Alert>
       </Row>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Exclusive Offers!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Here are the best offers for you!</p>
+          {'Buy 2 get 1 free'}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Go to Shop
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
